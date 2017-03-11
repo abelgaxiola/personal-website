2471f608326b88698679fbdbcd8b98d26a5f96b5
Abel Gaxiola <abelgaxiola@outlook.com>
2017-03-11 04:10:36 -0700

    Update app module with component imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
+import { BlogArchiveComponent } from './blog-archive/blog-archive.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    BlogListComponent,
+    BlogArchiveComponent,
+    BlogDetailComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
