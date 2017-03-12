17f62cc4ee571ab1cdbf1183b720c3c33fb6ecd0
Abel Gaxiola <abelgaxiola@outlook.com>
2017-03-11 21:08:11 -0700

    Create header component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,14 +2,6 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'footer-app',
-  template: `
-      <footer class="mdl-mini-footer">
-      <div class="mdl-mini-footer--left-section">
-        <button class="mdl-mini-footer--social-btn social-btn social-btn__twitter">
-              <span class="visuallyhidden">Twitter</span>
-            </button>
-      </div>
-    </footer>
-`
+  templateUrl: './footer.component.html'
 })
 export class FooterComponent{}
